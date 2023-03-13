5522ff8d62239d180f7cfd87b17d78afd3cf670e
tombazan <tomasbazan95@gmail.com>
2023-03-13 17:40:47 -0300

    probando tamaños en etiquetas

--- admin/etiquetarMasivo.php
+++ admin/etiquetarMasivo.php
@@ -21,7 +21,7 @@ if(empty($_SESSION['user'])){
 	
 	$array = explode(',', $_GET['id']);
 	echo "<div class='contenedor' style='max-width:100%; margin: 0;'>";
-	echo "<div style='display: grid; grid-template-columns: repeat(2, 1fr); justify-content: space-evenly; gap: 20px;'>";
+	echo "<div style='display: grid; grid-template-columns: repeat(2, 1fr); justify-content: space-evenly; gap: 30px;'>";
 	foreach ($array as $value)	{
 		
 		$sql = "SELECT `codigo`, `descripcion`, `precio`, `cb` FROM `productos` WHERE id = ? ";
@@ -36,11 +36,10 @@ if(empty($_SESSION['user'])){
 		
 		echo "<div class='row' style='border: solid 1px black; border-radius: 10px;'>";
 		echo "<div class='col'>";
-		echo "<p style='text-align: center; margin: 0; font-size: 20px;'>".$codigo."</p>";
-		echo "<div style='display:flex; justify-content: center;'>";
-		echo "<div><img style='max-width:100%; display: block; ' alt='testing' src='barcode.php?codetype=Code39&size=50&text=         ".$cb."&print=true'/></div>";
+		echo "<div style='display:flex; justify-content: center; height: 100px; flex-wrap: wrap; margin: 20px;'>";
+		echo "<div><img style='max-width:100%; max-height:100%; display: block; height: 120px; align-items: center;' alt='testing' src='barcode.php?codetype=Code39&size=50&text=         ".$cb."&print=true'/></div>";
 		echo "</div>";
-		echo "<p style='text-align: center; margin: 0; font-size: 20px;'>$".number_format($precio,2)."</p>";
+		echo "<p style='text-align: center; margin: 0; font-size: 25px; margin-bottom: 20px;'>".$codigo." $".number_format($precio,2)."</p>";
 		echo "</div>";
 		echo "</div>";
 
