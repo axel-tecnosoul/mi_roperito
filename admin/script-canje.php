2380362302ebac487de60b78c616eede83f93d67
Axel <axel@tecnosoul.com.ar>
2023-05-18 21:01:28 -0300

    al registrar un canje a la proveedora se le descuenta el total con descuento. Le aumentamos el credito a las proveedoras al ejecutar el script para obtener la deuda con las proveedoras

--- admin/script-canje.php
+++ admin/script-canje.php
@@ -5,7 +5,7 @@ require 'funciones.php';
 
 $pdo = Database::connect();
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "SELECT id, subtotal, id_modalidad, deuda_proveedor FROM canjes_detalle WHERE deuda_proveedor < 1";                             
+$sql = "SELECT cd.id, cd.subtotal, cd.id_modalidad, cd.deuda_proveedor, p.id_proveedor FROM canjes_detalle cd LEFT JOIN productos p ON cd.id_producto=p.id LEFT JOIN canjes c ON cd.id_canje=c.id";
 $q = $pdo->prepare($sql);
 $q->execute();
 $canje_detalle = $q->fetchAll(PDO::FETCH_ASSOC);
@@ -13,12 +13,24 @@ foreach ($canje_detalle as $data){
     $forma_pago = 1;
     $modalidad = $data['id_modalidad'];
     $subtotal = $data['subtotal'];
+    $idProveedor = $data['id_proveedor'];
     $deuda_proveedor=calcularDeudaProveedor($forma_pago,$modalidad,$subtotal);
 
     $id = $data['id'];
     $sql = "UPDATE canjes_detalle set deuda_proveedor = ? WHERE id = ?";
     $q = $pdo->prepare($sql);
     $q->execute(array($deuda_proveedor, $id));
+
+    if($modalidad==50){
+
+      $sql = "UPDATE canjes_detalle set pagado = 1 WHERE id = ?";
+      $q = $pdo->prepare($sql);
+      $q->execute(array($id));
+
+      $sql = "UPDATE proveedores set credito = credito + ? where id = ?";
+      $q = $pdo->prepare($sql);
+      $q->execute(array($deuda_proveedor,$idProveedor));
+    }
 }
 $pdo = Database::disconnect();
 ?>
