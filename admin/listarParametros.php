7896d45b78244b600b2fab7e09d3d794c9a8c024
Axel <axel@tecnosoul.com.ar>
2023-05-25 18:21:17 -0300

    se añade el modulo de parametros con el monto para facturacion electronica sin informar DNI. Se ponen los modulos de usuarios y empleados en el menu de maestros. Se agrega la posibilidad de actualizar los precios de forma masiva, ingresando un porcentaje y un valor de redondeo superior. Se agrega un control al generar una nueva venta para verificar si el monto a pagar es superior al monto configurado solicite el DNI como un campo obligatorio

---
+++ admin/listarParametros.php
@@ -0,0 +1,174 @@
+<?php
+session_start();
+if(empty($_SESSION['user'])) {
+  header("Location: index.php");
+  die("Redirecting to index.php");
+}?>
+<!DOCTYPE html>
+<html lang="en">
+  <head><?php
+    include('head_tables.php');?>
+  </head>
+  <body class="light-only">
+    <!-- page-wrapper Start-->
+    <div class="page-wrapper">
+      <!-- Page Header Start--><?php
+      include('header.php');?>
+      <!-- Page Header Ends                              -->
+      <!-- Page Body Start-->
+      <div class="page-body-wrapper">
+        <!-- Page Sidebar Start--><?php
+        include('menu.php');?>
+        <!-- Page Sidebar Ends-->
+        <div class="page-body">
+          <!-- Container-fluid starts-->
+          <div class="container-fluid">
+            <div class="page-header">
+              <div class="row">
+                <div class="col-10">
+                  <div class="page-header-left">
+                    <h3><?php include("title.php"); ?></h3>
+                    <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="#"><i data-feather="home"></i></a></li>
+                      <li class="breadcrumb-item">Parametros</li>
+                    </ol>
+                  </div>
+                </div>
+                <!-- Bookmark Start-->
+                <div class="col-2">
+                  <div class="bookmark pull-right">
+                    <ul>
+                      <li><a  target="_blank" data-container="body" data-toggle="popover" data-placement="top" title="" data-original-title="<?php echo date('d-m-Y');?>"><i data-feather="calendar"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- Bookmark Ends-->
+              </div>
+            </div>
+          </div>
+          <div class="container-fluid">
+            <div class="row">
+              <!-- Zero Configuration  Starts-->
+              <div class="col-sm-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h5>Parametros</h5>
+                  </div>
+                  <div class="card-body">
+                    <div class="dt-ext table-responsive">
+                      <table class="display" id="dataTables-example666">
+                        <thead>
+                          <tr>
+                            <!-- <th>ID</th> -->
+                            <th>Parametro</th>
+                            <th>Valor</th>
+                            <th>Opciones</th>
+                          </tr>
+                        </thead>
+                        <tbody><?php
+                          include 'database.php';
+                          $pdo = Database::connect();
+                          $sql = " SELECT id, parametro, valor FROM parametros WHERE id=6 ";
+                          
+                          foreach ($pdo->query($sql) as $row) {
+                              echo '<tr>';
+                              //echo '<td>'.$row[0].'</td>';
+                              echo '<td>'.$row[1].'</td>';
+                              echo '<td>';
+                              if(is_numeric($row[2])){
+                                echo number_format($row[2],0,",",".");
+                              }else{
+                                echo $row[2];
+                              }
+                              echo '</td>';
+                              echo '<td>';
+                              echo '<a href="modificarParametro.php?id='.$row[0].'"><img src="img/icon_modificar.png" width="24" height="25" border="0" alt="Modificar" title="Modificar"></a>';
+                              echo '&nbsp;&nbsp;';
+                              echo '</td>';
+                              echo '</tr>';
+                          }
+                          Database::disconnect();?>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Zero Configuration  Ends-->
+              <!-- Feature Unable /Disable Order Starts-->
+            </div>
+          </div>
+          <!-- Container-fluid Ends-->
+        </div>
+        <!-- footer start--><?php
+        include("footer.php"); ?>
+      </div>
+    </div>
+    <!-- latest jquery-->
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap js-->
+    <script src="assets/js/bootstrap/popper.min.js"></script>
+    <script src="assets/js/bootstrap/bootstrap.js"></script>
+    <!-- feather icon js-->
+    <script src="assets/js/icons/feather-icon/feather.min.js"></script>
+    <script src="assets/js/icons/feather-icon/feather-icon.js"></script>
+    <!-- Sidebar jquery-->
+    <script src="assets/js/sidebar-menu.js"></script>
+    <script src="assets/js/config.js"></script>
+    <!-- Plugins JS start-->
+    <script src="assets/js/datatable/datatables/jquery.dataTables.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.buttons.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/jszip.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/buttons.colVis.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/pdfmake.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/vfs_fonts.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.autoFill.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.select.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/buttons.bootstrap4.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/buttons.html5.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/buttons.print.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.responsive.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/responsive.bootstrap4.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.keyTable.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.colReorder.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.fixedHeader.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.rowReorder.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/dataTables.scroller.min.js"></script>
+    <script src="assets/js/datatable/datatable-extension/custom.js"></script>
+    <script src="assets/js/chat-menu.js"></script>
+    <script src="assets/js/tooltip-init.js"></script>
+    <!-- Plugins JS Ends-->
+    <!-- Plugins JS Ends-->
+    <!-- Theme js-->
+    <script src="assets/js/script.js"></script>
+    <script>
+      $(document).ready(function() {
+        $('#dataTables-example666').DataTable({
+          stateSave: true,
+          responsive: true,
+          language: {
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Registros",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Registros",
+            "infoFiltered": "(Filtrado de _MAX_ total registros)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Registros",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "No hay resultados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+          }
+        });
+      });
+    </script>
+  </body>
+</html>
