76495af1ffe920014c57ee21b104e12f8c66a78e
tombazan <tomasbazan95@gmail.com>
2023-06-16 22:46:03 -0300

    siguiendo con la maquetacion del HOME y NOSOTROS.Subiendo para testear

---
+++ nueva_web/header.php
@@ -0,0 +1,144 @@
+<meta http-equiv="refresh" content="1000;url=index.php" />
+	
+	<!-- tt-mobile menu -->
+	<nav class="panel-menu mobile-main-menu">
+		<ul>
+			<li>
+				<a href="index.php">Inicio</a>
+			</li>
+			<li>
+				<!-- <a href="comprar.php">Comprá</a> -->
+        <a href="http://miroperito.prestotienda.com/">Comprá</a>
+			</li>
+			<li>
+				<a href="vender.php">Vendé</a>
+			</li>
+			<li>
+				<a href="nosotros.php">Nosotros</a>
+			</li>
+		</ul>
+		<div class="mm-navbtn-names">
+			<div class="mm-closebtn">Cerrar</div>
+			<div class="mm-backbtn">Volver</div>
+		</div>
+	</nav>
+	<!-- tt-mobile-header -->
+	<div class="tt-mobile-header">
+		<div class="container-fluid">
+			<div class="tt-header-row">
+				<div class="tt-mobile-parent-menu">
+					<div class="tt-menu-toggle" id="js-menu-toggle">
+						<i class="icon-03"></i>
+					</div>
+				</div>
+				<!-- search -->
+				<div class="tt-mobile-parent-search tt-parent-box"></div>
+				<!-- /search -->
+				<!-- cart -->
+				<div class="tt-mobile-parent-cart tt-parent-box"></div>
+				<!-- /cart -->
+				<!-- account -->
+				<div class="tt-mobile-parent-account tt-parent-box"></div>
+				<!-- /account -->
+			</div>
+		</div>
+		<div class="container-fluid tt-top-line">
+			<div class="row">
+				<div class="tt-logo-container">
+					<!-- logo -->
+					<a class="tt-logo tt-logo-alignment" href="index.php"><img src="images/logo/logo-nuevom.png" alt="" style="max-height: 80px!important; margin-left:0; margin-top: 20px;"></a>
+					<!-- /logo -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- tt-desktop-header -->
+	<br>
+	
+	<div class="tt-desktop-header">
+		<div class="container-fluid">
+			<div class="tt-header-holder" style="flex-direction: column!important;">
+				<div class="tt-obj-logo">
+					<!-- logo -->
+					<a class="tt-logo tt-logo-alignment" href="index.php"><img src="images/logo/logo-nuevom.png" alt="" style="max-height: 120px!important; margin-left:0;"></a>
+					<!-- /logo -->
+				</div>
+				<div class="tt-obj-menu">
+					<div class="container">
+						<div class="row justify-content-md-center">
+							<!-- tt-menu -->
+							<div class="tt-desctop-parent-menu tt-parent-box">
+								<div class="tt-desctop-menu">
+									<nav>
+										<ul>
+											<li class="dropdown">
+												<a href="index.php"><span>INICIO</span></a>
+											</li>
+											<p style="margin: 0; padding: 0; color: #FF33F9; font-weight: bold;">I</p>
+											<li class="dropdown">
+												<!-- <a href="comprar.php"><span>Comprá</span></a> -->
+                        <a href="http://miroperito.prestotienda.com/"><span>COMPRÁ</span></a>
+											</li>
+											<p style="margin: 0; padding: 0; color: #21BD21; font-weight: bold;">I</p>
+											<li class="dropdown">
+												<a href="vender.php"><span>VENDÉ</span></a>
+											</li>
+											<p style="margin: 0; padding: 0; color: #FF33F9; font-weight: bold;">I</p>
+											<li class="dropdown">
+												<a href="nosotros.php"><span>NOSOTROS</span></a>
+											</li>
+											<p style="margin: 0; padding: 0; color: #21BD21; font-weight: bold;">I</p>
+											<li class="dropdown">
+												<a href="locales.php"><span>LOCALES</span></a>
+											</li>
+										</ul>
+									</nav>
+								</div>
+							</div>
+							<!-- /tt-menu -->
+						</div>
+					</div>
+				</div>
+				<div class="tt-obj-options obj-move-right tt-position-absolute">
+					<div class="tt-desctop-parent-account tt-parent-box">
+						<div class="tt-account tt-dropdown-obj">
+							<button class="tt-dropdown-toggle"  data-tooltip="Mi Cuenta" data-tposition="bottom"><i class="icon-f-94"></i></button>
+							<div class="tt-dropdown-menu">
+								<div class="tt-mobile-add">
+									<button class="tt-close">Cerrar</button>
+								</div>
+								<div class="tt-dropdown-inner">
+									<ul>
+									<li><a target="_blank" href="../admin/loginProveedores.php"><i class="icon-f-76"></i>Ingresar</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- /tt-account -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- stuck nav -->
+	<div class="tt-stuck-nav stuck-layout-02" id="js-tt-stuck-nav">
+		<div class="container-fluid">
+			<div class="tt-header-row">
+				<div class="col-left col-12 col-lg-2">
+					<div class="tt-stuck-parent-logo">
+						
+						<a class="tt-logo tt-logo-alignment" href="index.php"><img src="images/logo/logo-nuevom.png" alt="" style="max-height: 50px!important; margin-left:10px;"></a>
+						
+					</div>
+				</div>
+				<div class="col-center col-4 col-lg-8">
+					<div class="tt-stuck-parent-menu"></div>
+				</div>
+				<div class="col-right col-8 col-lg-2">
+					<div class="tt-stuck-parent-search tt-parent-box"></div>
+					<div class="tt-stuck-parent-cart tt-parent-box"></div>
+					<div class="tt-stuck-parent-account tt-parent-box"></div>
+				</div>
+			</div>
+		</div>
+	</div>
