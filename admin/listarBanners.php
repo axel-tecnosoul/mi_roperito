cb4d99c827b8ada2501c2a511cb462d9642c3707
tombazan <tomasbazan95@gmail.com>
2023-09-18 15:48:13 -0300

    finalizado abm de Banners

--- admin/listarBanners.php
+++ admin/listarBanners.php
@@ -87,12 +87,14 @@ if(empty($_SESSION['user'])) {
                               echo '<td>NO</td>';
                             }
                             echo '<td>';
+                            echo '<a href="#" data-toggle="modal" data-target="#imagenModal' . $row['id'] . '" data-imagen="' . $row['url-jpg'] . '" data-seccion="' . $row['seccion'] . '">
+                            <img src="img/eye.png" id="mostrar_imagen" width="24" height="15" border="0" alt="Ver" title="Ver Imagen">
+                            </a>';
+                            echo '&nbsp;&nbsp;';
                             echo '<a href="modificarBanner.php?id='.$row['id'].'"><img src="img/icon_modificar.png" width="24" height="25" border="0" alt="Modificar" title="Modificar"></a>';
                             echo '&nbsp;&nbsp;';
-                            echo '<a href="#" data-toggle="modal" data-target="#imagenModal' . $row['id'] . '" data-imagen="' . $row['url-jpg'] . '" data-seccion="' . $row['seccion'] . '">
-                              <img src="img/eye.png" id="mostrar_imagen" width="24" height="15" border="0" alt="Ver" title="Ver Imagen">
-                            </a>';
-
+                            echo '<a href="#" data-toggle="modal" data-target="#eliminarModal_'.$row['id'].'"><img src="img/icon_baja.png" width="24" height="25" border="0" alt="Eliminar" title="Eliminar"></a>';
+                              echo '&nbsp;&nbsp;';
                             echo '</td>';
                             echo '</tr>';
                                 
@@ -133,6 +135,30 @@ if(empty($_SESSION['user'])) {
         include("footer.php"); ?>
       </div>
     </div>
+    <?php 
+    $pdo = Database::connect();
+    $sql = " SELECT `id`, seccion, `url-jpg`, activo FROM `banners` WHERE 1 ";
+    foreach ($pdo->query($sql) as $row) {
+    ?>
+    <div class="modal fade" id="eliminarModal_<?php echo $row['id'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirmación</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+        </div>
+        <div class="modal-body">¿Está seguro que desea eliminar el Banner?</div>
+        <div class="modal-footer">
+        <a href="eliminarBanner.php?id=<?php echo $row['id'];?>" class="btn btn-primary">Eliminar</a>
+        <a onclick="document.location.href='listarBanners.php'" class="btn btn-light">Volver</a>
+        </div>
+      </div>
+      </div>
+    </div>
+    <?php 
+    }
+    Database::disconnect();
+    ?>
     <!-- latest jquery-->
     <script src="assets/js/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap js-->
