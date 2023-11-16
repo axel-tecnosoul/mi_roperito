7e1bc13457e8b1dd88288350f59eb1faedb8f9d4
tombazan <tomasbazan95@gmail.com>
2023-11-16 10:17:07 -0300

    correcciones en pdf y convenio

--- nueva_web/btn-convenio.php
+++ nueva_web/btn-convenio.php
@@ -1,7 +1,19 @@
 <section class="container btn-convenio">
 	<div>
-		<a href="../admin/files/Convenio.pdf" target="_blank">Ver convenio</a>
-		<a href="turnos.php" class="segundo-enlace" target="_blank">Sacar turno para vender</a>
+		<?php 
+		include '../admin/database.php';
+		$pdo = Database::connect();
+		$id = 9;
+  		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT id,parametro,valor FROM parametros WHERE id = ? ";
+		$q = $pdo->prepare($sql);
+		$q->execute([$id]);
+		$data = $q->fetch(PDO::FETCH_ASSOC);
+
+		Database::disconnect();
+		?>
+		<a href="../admin/files/<?=$data["valor"];?>" target="_blank">Ver convenio</a>
+		<a href="turnos.php" class="segundo-enlace">Sacar turno para vender</a>
 		<a href="../admin/loginProveedores.php" target="_blank">Ver el estado de mis ventas</a>
 	</div>
 </section>
