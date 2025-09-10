49f733d3435c643451fb7411102d3d3a2ae88110
Axel Britzius <axelbritzius@gmail.com>
2025-09-10 10:05:09 -0300

    Refactor email sending logic and add debug mode for specific email

---
+++ comprar.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include("head.php"); ?>
+</head>
+<body>
+<div id="loader-wrapper">
+	<div id="loader">
+		<div class="dot"></div>
+		<div class="dot"></div>
+		<div class="dot"></div>
+		<div class="dot"></div>
+		<div class="dot"></div>
+		<div class="dot"></div>
+		<div class="dot"></div>
+	</div>
+</div>
+<header id="tt-header">
+	<?php include("header.php"); ?>
+</header>
+<br>
+<section>
+<div class="container">
+	<div class="row">
+  
+	  
+  
+	</div>
+</section>
+<br>
+<br>
+  </div>
+	<div class="tt-pageContent">
+		
+		<div class="container-fluid-custom">
+			
+			<div class="row">
+				<div class="col-12 centrar-div">
+					<h3>NUESTRA MISIÓN</h3>
+					<p class="">Extender el ciclo de vida de las prendas de marca.</p>
+					<h3>NUESTRA VISIÓN</h3>
+					<p class="">Una verdadera comunidad dedicada a un futuro mas sustentable</p>
+				  </div>
+				  <div class="container">
+					<div class="col-12 centrar-div">
+						<p class="">Se siguen tomando prendas de todas las temporadas y se cotiza como estación actual las prendas de <a href>Primavera-Verano.</a></p>
+					  </div>
+					<div class="well">
+						<h3 class="text-divider"><span>Visitá nuestras sucursales</span></h3>
+						<div class="container">
+							<div class="row">
+						  
+							  <div class="col-md">
+								<div class="card text-center">
+									
+									<div class="card-body">
+									  <h1 class="card-title"><i class="fas fa-map-marker-alt"></i></h1>
+									  <a href="#" class="btn2">Villa Ballester</a>
+									  
+									</div>
+									<div class="card-footer text-muted">Independencia 4701, Planta alta</div>
+									<img class="img01" src="images/locales/ballester.jpg" alt="">
+								  </div>
+							  </div>
+						  
+							  <div class="col-md">
+								<div class="card text-center">
+									
+									<div class="card-body">
+									  <h1 class="card-title"><i class="fas fa-map-marker-alt"></i></h1>
+									  <a href="#" class="btn2">San Isidro</a>
+									</div>
+									<div class="card-footer text-muted">Avenida Fondo de la Legua 425, Planta alta</div>
+									<img class="img01" src="images/locales/sanisidro.jpg" alt="">
+								  </div>
+							  </div>
+						  
+							  <div class="col-md">
+								<div class="card text-center">
+									
+									<div class="card-body">
+									  <h1 class="card-title"><i class="fas fa-map-marker-alt"></i></h1>
+									  <a href="#" class="btn2">Martinez</a>
+									</div>
+									<div class="card-footer text-muted">Parana 3922</div>
+									<img class="img01" src="images/locales/martinez.jpg" alt="">
+								  </div>
+								  </div>
+								  
+							  </div>
+						  
+							 
+						  
+							</div>
+						  </div>
+					</div>
+					</div>
+					<br>
+					<br>
+					
+		
+					<hr class="style13">
+			
+			<br>
+			<br>
+			<br>
+			<br>
+			<?php include("testimonios.php"); ?>
+				
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+			
+		<hr class="style13">
+		<br>
+		<br>
+		<br>
+		<br>
+
+			<?php include("suscribite.php"); ?>
+<br>
+<br>
+<br>
+<footer id="">
+	<?php include("footer.php"); ?>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="external/jquery/jquery.min.js"><\/script>')</script>
+<script defer src="js/bundle.js"></script>
+
+<script defer src="separate-include/single-product/single-product.js"></script>
+<script src="separate-include/portfolio/portfolio.js"></script>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+
+<a href="#" class="tt-back-to-top">Volver al inicio</a>
+</body>
+</html>
