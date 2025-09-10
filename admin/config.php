bf7a85a3756409ced49726bb6d8d74cdfd2ad7f1
Axel <axelbritzius@gmail.com>
2025-09-10 09:37:36 -0300

    Respect existing reCAPTCHA keys

--- admin/config.php
+++ admin/config.php
@@ -7,4 +7,12 @@ $smtpUsuario = getenv('SMTP_USER') ?: 'avisos@miroperito.ar';
 $smtpClave = getenv('SMTP_PASS') ?: '';
 $fromEmail = getenv('SMTP_FROM') ?: 'avisos@miroperito.ar';
 $fromName  = getenv('SMTP_FROM_NAME') ?: 'MiRoperito';
+
+// reCAPTCHA keys; respect values already defined and fall back to environment variables
+if (!isset($recaptchaSiteKey)) {
+    $recaptchaSiteKey = getenv('RECAPTCHA_SITE_KEY') ?: '';
+}
+if (!isset($recaptchaSecret)) {
+    $recaptchaSecret = getenv('RECAPTCHA_SECRET') ?: '';
+}
 ?>
