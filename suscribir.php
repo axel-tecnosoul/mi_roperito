0e85ae0ae6f0d9415fb3e3acc0df7f46d17b8247
Axel <axelbritzius@gmail.com>
2025-09-10 08:07:05 -0300

    Fix subscription redirect

--- suscribir.php
+++ suscribir.php
@@ -1,20 +1,21 @@
-<?php 
-    require("admin/config.php");
-	require("admin/database.php");
-    
-	$pdo = Database::connect();
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-	$sql = "SELECT `id` from suscripciones where email = ?";
-	$q = $pdo->prepare($sql);
-	$q->execute(array($_POST['email']));
-	$data = $q->fetch(PDO::FETCH_ASSOC);
-	if (empty($data)) {
-		$sql = "INSERT INTO `suscripciones`(`email`, `fecha_hora`) VALUES (?,now())";
-		$q = $pdo->prepare($sql);
-		$q->execute(array($_POST['email']));
+<?php
+require("admin/config.php");
+require("admin/database.php");
 
-		Database::disconnect();		
-	}
-	header("Location: ../nueva_web/index.php");
+$pdo = Database::connect();
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "SELECT `id` from suscripciones where email = ?";
+$q = $pdo->prepare($sql);
+$q->execute(array($_POST['email']));
+$data = $q->fetch(PDO::FETCH_ASSOC);
+if (empty($data)) {
+    $sql = "INSERT INTO `suscripciones`(`email`, `fecha_hora`) VALUES (?,now())";
+    $q = $pdo->prepare($sql);
+    $q->execute(array($_POST['email']));
+
+    Database::disconnect();
+}
+header("Location: index.php");
 ?>
+
