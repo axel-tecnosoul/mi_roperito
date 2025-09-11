c4f46b34c63d75c215f9ea1ac8ab4b8def3aab52
Axel <axelbritzius@gmail.com>
2025-09-11 09:13:19 -0300

    Disable submit button while sending contact form

--- js/contacto.js
+++ js/contacto.js
@@ -2,12 +2,15 @@ $(function() {
     $('#contactForm').on('submit', function(e) {
         e.preventDefault();
         var $form = $(this);
+        var $submitBtn = $form.find('button[type="submit"]');
+        var originalText = $submitBtn.text();
         var recaptchaResponse = grecaptcha.getResponse();
         if (!recaptchaResponse) {
             $('#contact-message-text').text('Por favor, verifica el reCAPTCHA.');
             $('#contact-message').modal('show');
             return;
         }
+        $submitBtn.prop('disabled', true).text('Enviando...');
         $.ajax({
             url: $form.attr('action'),
             type: 'POST',
@@ -16,6 +19,7 @@ $(function() {
         }).done(function(resp) {
             $('#contact-message-text').text(resp.message);
             $('#contact-message').modal('show');
+            $submitBtn.prop('disabled', false).text(originalText);
             if (resp.success) {
                 $form[0].reset();
                 grecaptcha.reset();
@@ -23,6 +27,7 @@ $(function() {
         }).fail(function() {
             $('#contact-message-text').text('No se pudo enviar el mensaje.');
             $('#contact-message').modal('show');
+            $submitBtn.prop('disabled', false).text(originalText);
         });
     });
 });
